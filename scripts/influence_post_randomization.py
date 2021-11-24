4d6cd7d52f359b0ee05dd8acaa8de16ddb534e4b
Sandeep Soni <sandeepsoni@gatech.com>
2021-11-24 12:35:23 -0500

    modify the API calls

--- scripts/influence_post_randomization.py
+++ scripts/influence_post_randomization.py
@@ -21,8 +21,8 @@ def readArgs ():
 	parser.add_argument ("--data-file", type=str, required=True, help="The name of the file containing all the data")
 	parser.add_argument ("--params-file", type=str, required=True, help="The name of the file containing all the params")
 	parser.add_argument ("--bandwidth", type=float, required=False, default=2.0, help="Bandwidth parameter")
+	parser.add_argument ("--ncascades", type=int, required=False, default=None, help="The number of cascades to consider")
 	parser.add_argument ("--seed", type=int, required=False, default=42, help="random seed")
-	#parser.add_argument ("--train-percent", type=float, required=False, default=0.8, help="train percentage in a train-test split")
 	args = parser.parse_args ()
 	return args
 
@@ -55,28 +55,15 @@ def main (args):
 		idx, iidx, cascades, innovs = pickle.load (fin)
 
 	# Randomize the cascades
-	randomized_cascades = randomize_cascades (cascades, args.seed)
-
-	# Divide the cascades into a random train and dev
-	#random.seed (args.seed)
-	#random.shuffle (cascades)
-	#random.seed (args.seed)
-	#random.shuffle (innovs)
-	#train_cascades = cascades[:int((len(cascades)+1)*args.train_percent)]
-	#test_cascades = cascades[int((len(cascades)+1)*args.train_percent):] 
+	randomized_cascades = randomize_cascades (cascades[0:args.ncascades], args.seed)
 	
 	dims = len (idx)
 	logging.info (f'Number of channels in the cascades: {dims}')
-	#mu,b,c,s = hpmodels.DCHP.estimate (train_cascades, hpmodels.DCHP.log_likelihood_many_cascades, bandwidth=args.bandwidth, dims=dims)
 
-	#params = np.concatenate ((mu, b, c, s))
-	#heldout_log_likelihood = hpmodels.DCHP.log_likelihood_many_cascades (params, test_cascades, bandwidth=args.bandwidth, dims=dims, sign=1.0)	
-	#logging.info (f'Held out log likelihood: {heldout_log_likelihood}')
-
-	mu,b,c,s = hpmodels.DCHP.estimate (randomized_cascades, hpmodels.DCHP.log_likelihood_many_cascades, bandwidth=args.bandwidth, dims=dims)
+	result = hpmodels.DCHP.estimate (randomized_cascades, hpmodels.DCHP.log_likelihood_many_cascades, hpmodels.DCHP.log_likelihood_single_cascade, bandwidth=args.bandwidth, dims=dims, seed=42)
 	
-	with open(args.params_file,'wb') as f: pickle.dump((mu, b, c, s), f)
-	logging.info (f'Coarsened HP inference done, parameters written in file {args.params_file}')
+	with open(args.params_file,'wb') as f: pickle.dump(result, f)
+	logging.info (f'Inference done, parameters written in file {args.params_file}')
 	
 if __name__ == "__main__":
 	main (readArgs())
