b37491f2f89227f693ad39d95358ef52f4275594
Sandeep Soni <sandeepsoni@gatech.com>
2022-01-01 16:04:34 -0500

    add python and slurm script for filters

---
+++ scripts/pre-measurement-word-filters/by_frequency.py
@@ -0,0 +1,44 @@
+"""
+
+Before computing the first and the second moment, we filter down the vocabulary by keeping words that:
+
+- are mentioned at least 30 times
+
+"""
+
+import argparse
+import logging
+from tqdm import tqdm
+from collections import defaultdict
+
+logging.basicConfig(
+	format='%(asctime)s %(levelname)-8s %(message)s',
+	level=logging.INFO,
+	datefmt='%Y-%m-%d %H:%M:%S')
+
+def readArgs ():
+	parser = argparse.ArgumentParser (description="Filter down the vocabulary")
+	parser.add_argument ("--input-embeddings-file", required=True, type=str, help="Single embeddings file")
+	parser.add_argument ("--min-freq", required=False, type=int, default=30, help="Minimum frequency")
+	parser.add_argument ("--keep-file", required=True, type=str, help="Words that pass the filter")
+	parser.add_argument ("--discard-file", required=True, type=str, help="Words that fail the filter")
+	args = parser.parse_args ()
+	return args
+
+def main (args):
+	vocab_counts = defaultdict (int)
+	with open (args.input_embeddings_file) as fin:
+		for i, line in enumerate (tqdm(fin)):
+			parts = line.strip().split ("\t")
+			word = parts[3]
+			vocab_counts[word] += 1
+
+	with open (args.keep_file, "w") as keep_file, open (args.discard_file, "w") as discard_file:
+		for word in vocab_counts:
+			if vocab_counts[word] >= args.min_freq:
+				keep_file.write (f"{word}\n")
+			else:
+				discard_file.write (f"{word}\n")
+
+if __name__ == "__main__":
+	main (readArgs ())
