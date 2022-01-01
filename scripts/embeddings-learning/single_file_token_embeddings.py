5499aa062e308261073d6ade199288ef9b11b926
Sandeep Soni <sandeepsoni@berkeley.edu>
2022-01-01 02:42:11 -0800

    add python and slurm script to aggregate the token embeddings

---
+++ scripts/embeddings-learning/single_file_token_embeddings.py
@@ -0,0 +1,41 @@
+import argparse
+import json
+import logging
+from tqdm import tqdm
+
+logging.basicConfig(
+	format='%(asctime)s %(levelname)-8s %(message)s',
+	level=logging.INFO,
+	datefmt='%Y-%m-%d %H:%M:%S')
+
+def readArgs ():
+	parser = argparse.ArgumentParser (description="Take multiple files and join them into one after including other fields")
+	parser.add_argument ("--filenames", type=str, required=True, nargs="+", help="Files contain token embeddings on a sample")
+	parser.add_argument ("--output-file", type=str, required=True, help="File will be output to contain all the embeddings")
+	parser.add_argument ("--metadata-file", type=str, required=True, help="The metadata file contains additional fields")
+	args = parser.parse_args ()
+	return args
+
+def main (args):
+	paper_ids = dict ()
+	with open (args.metdata_file) as fin:
+		for line in fin:
+			js = json.loads (line)
+			if js["paper_id"] not in paper_ids:
+				paper_ids[js["paper_id"]] = js["metadata"]["year"]
+
+	with open (args.output_file, "w") as fout:
+		for filename in args.filenames:
+			with open (filename) as fin:
+				for i, line in enumerate (tqdm (fin)):
+					parts = line.strip().split ("\t") # split on tab
+					paper_id = parts[0]
+					year = paper_ids[paper_id]
+					output_string = "\t".join ([paper_id, str(year)] + parts[1:])
+					fout.write (f"{output_string}\n")
+
+					if (i+1) % 10000 == 0:
+						logging.info (f"{i+1} tokens in {filename} processed")
+
+if __name__ == "__main__":
+	main (readArgs ())
---
+++ slurm_scripts/single_file_token_embeddings.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Job name: Give your job a meaningful name
+
+#SBATCH --job-name=acl_bert_embeddings_aggregate
+
+# Partition: choose the correct machine, and below you can specify the number of nodes and tasks. 
+# This is a magical mystery resolved by reading savio documentation or consulting with their help desk, e.g. https://docs-research-it.berkeley.edu/services/high-performance-computing/user-guide/hardware-config/
+
+#SBATCH --partition=savio3 
+
+
+# All the other options
+
+#SBATCH --time=3-00:00:00
+
+#SBATCH --account=fc_dbamman
+
+#SBATCH --nodes=1
+
+#SBATCH --ntasks=1
+
+#SBATCH --cpus-per-task=4
+
+#SBATCH --mail-user=sandeepsoni@berkeley.edu
+
+#SBATCH --mail-type=all
+
+HOME_DIR=/global/home/users/sandeepsoni
+USERS_DIR=/global/scratch/users/sandeepsoni
+PROJECTS_DIR=$HOME_DIR/projects/hp-modeling
+SCRATCH_DIR=$USERS_DIR/projects/hp-modeling
+
+conda activate $USERS_DIR/envs/py39/
+
+export TRANSFORMERS_CACHE="/global/scratch/users/sandeepsoni/models/transformers" # Keep all the models in this directory
+export HF_DATASETS_CACHE="/global/scratch/users/sandeepsoni/datasets/transformers" # Keep all the datasets in this directory
+
+cd $PROJECTS_DIR/scripts/embeddings-learning/
+module load cuda/10.2
+hostname
+which python
+python forward-pass.py --text-file $SCRATCH_DIR/data/raw/sample.jsonl --model-checkpoint $SCRATCH_DIR/checkpoints/contextual-word-embeddings/checkpoint-9000 --embeddings-file $SCRATCH_DIR/data/contextual-embeddings/sample.tsv
+
+python single_file_token_embeddings.py --filenames $SCRATCH_DIR/data/contextual-embeddings/s2orc_acl_chunks01.tsv $SCRATCH_DIR/data/contextual-embeddings/s2orc_acl_chunks02.tsv $SCRATCH_DIR/data/contextual-embeddings/s2orc_acl_chunks03.tsv $SCRATCH_DIR/data/contextual-embeddings/s2orc_acl_chunks04.tsv $SCRATCH_DIR/data/contextual-embeddings/s2orc_acl_chunks05.tsv $SCRATCH_DIR/data/contextual-embeddings/s2orc_acl_chunks06.tsv $SCRATCH_DIR/data/contextual-embeddings/s2orc_acl_chunks07.tsv $SCRATCH_DIR/data/contextual-embeddings/s2orc_acl_chunks08.tsv $SCRATCH_DIR/data/contextual-embeddings/s2orc_acl_chunks09.tsv $SCRATCH_DIR/data/contextual-embeddings/s2orc_acl_chunks10.tsv --output-file $SCRATCH_DIR/data/contextual-embeddings/s2orc_acl_embeddings.tsv --metadata-file $SCRATCH_DIR/data/raw/s2orc_acl_p4.jsonl
+
+conda deactivate
+#source deactivate
