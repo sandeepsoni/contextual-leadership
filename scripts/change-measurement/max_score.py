38b608978937c6df93d869cb0be4952f3a718b5c
Sandeep Soni <soni.sandeepb@gmail.com>
2022-01-10 15:52:09 -0800

    add max score script

--- scripts/change-measurement/max_score.py
+++ scripts/change-measurement/max_score.py
@@ -19,10 +19,10 @@ def main (args):
 		filename = os.path.join (args.word_embeddings_dir, word, f"{word}.computed_scores")
 		with open (filename) as fin:
 			parts = [line.strip().split("\t") for line in fin]
-			scores = [(int(part[1]), float (part[2])) for part in parts]
+			scores = [(int(part[1]), float (part[3])) for part in parts]
 			max_score = max (scores, key=lambda x:x[1])
 		
-		output_filename = os.path.join (args.word_embeddings_dir, word, f"{word}.frequency_accounting.max_score")
+		output_filename = os.path.join (args.word_embeddings_dir, word, f"{word}.no-frequency_accounting.max_score")
 		with open (output_filename, "w") as fout:
 			fout.write (f"{max_score[0]}\t{max_score[1]}\n")
 				
